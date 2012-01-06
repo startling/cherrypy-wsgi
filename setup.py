40f072ef47928c938739694c08476b7e7831e053
tim <tdixon51793@gmail.com>
2012-01-06 16:52:54 -0600

    Your basic setup.py. Nothin much.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "cherrypy-wsgi",
+    version = "0.0.0",
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    description = "A script for running wsgi applications from the command-line with cherrypy.",
+    scripts = ['cherrypy-wsgi'],
+    install_requires = ['cherrypy'],
+)
